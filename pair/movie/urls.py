eae14f55973f030312e71af25c5515b9621515db
BuildEnough <rhtmaehcl878@naver.com>
2022-09-30 11:04:12 +0900

    m2

--- pair/movie/urls.py
+++ pair/movie/urls.py
@@ -5,4 +5,6 @@ app_name = "movie"
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('create/', views.create, name='create'),
 ]
--- pair/movie/views.py
+++ pair/movie/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Review
 
 # Create your views here.
 
-def index(request):
 
-    return render(request, 'movie/index.html')
+def index(request):
+    _movie = Review.objects.all()
+
+    context = {
+        "movie": _movie,
+    }
+    return render(request, "movie/index.html", context)
+
+
+def create(request):
+    content = request.GET.get("content_")
+
+    Review.object.create(content=content)
+
+    return redirect("movie:index")
